7800c97f4caf58b02619cc6efaa72282f78e099b
Parimal Kesan <parimalkesan3@gmail.com>
2021-02-01 16:11:15 +0530

    Added DB code

---
+++ src/main/java/com/example/Sales_App/DAO/SalesRepMapper.java
@@ -0,0 +1,22 @@
+package com.example.Sales_App.DAO;
+
+import com.example.Sales_App.Model.SalesRep;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SalesRepMapper implements RowMapper<SalesRep> {
+
+    @Override
+    public SalesRep mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+        SalesRep salesRep = new SalesRep();
+        salesRep.setId(resultSet.getInt("Id"));
+        salesRep.setName(resultSet.getString("Name"));
+        salesRep.setCountry(resultSet.getString("Country"));
+        salesRep.setCity(resultSet.getString("City"));
+        salesRep.setZipCode(resultSet.getInt("Zipcode"));
+        salesRep.setGender(resultSet.getString("Gender"));
+        return salesRep;
+    }
+}
